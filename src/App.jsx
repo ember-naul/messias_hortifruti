90e83a4159ca69c11109a01969a10fbce0e1abac
embernow <lluann930@gmail.com>
2024-11-16 16:41:05 -0300

    Vercel.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, RouterProvider } from 'react-router-dom';
-import Home from './pages/Home';
-import CarrinhoCompras from './pages/CarrinhoCompras';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { router } from './routes/RoutesConfig';
--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,6 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App/>
-     
   </React.StrictMode>,
 )
 
